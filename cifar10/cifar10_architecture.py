7eb146de2ac7fd745fcd67b4e67930257336b331
clint <clint@clintui-MacBook-Pro.local>
2016-01-11 05:31:16 +0900

    apply elu non-linear function

--- cifar10/cifar10_architecture.py
+++ cifar10/cifar10_architecture.py
@@ -7,14 +7,14 @@ class Cifar10Architecture(object):
         self._session = session
 
     def forward(self, images):
-        conv1 = tf.nn.relu( elements._convolution('conv1', images, kernel_shape=[5, 5, 3, 64]), name='conv1')
+        conv1 = tf.nn.elu( elements._convolution('conv1', images, kernel_shape=[5, 5, 3, 64]), name='conv1')
         pool1 = elements._pooling('pool1', conv1, kernel_shape=[1, 3, 3, 1], strides=[1, 2, 2, 1])
         norm1 = elements._local_response_normalization('norm1', pool1)
-        conv2 = tf.nn.relu( elements._convolution('conv2', norm1,  kernel_shape=[5, 5, 64, 64]), name='conv2')
+        conv2 = tf.nn.elu( elements._convolution('conv2', norm1,  kernel_shape=[5, 5, 64, 64]), name='conv2')
         norm2 = elements._local_response_normalization('norm2', conv2)
         pool2 = elements._pooling('pool2', norm2, kernel_shape=[1, 3, 3, 1], strides=[1, 2, 2, 1])
-        local3 = tf.nn.relu( elements._fullconnect('local3', pool2,  hidden_nodes=384, stddev=0.04, wd=0.004), name='local3')
-        local4 = tf.nn.relu( elements._fullconnect('local4', local3, hidden_nodes=192, stddev=0.04, wd=0.004), name='local4')
+        local3 = tf.nn.elu( elements._fullconnect('local3', pool2,  hidden_nodes=384, stddev=0.04, wd=0.004), name='local3')
+        local4 = tf.nn.elu( elements._fullconnect('local4', local3, hidden_nodes=192, stddev=0.04, wd=0.004), name='local4')
         logits = elements._fullconnect('linear', local4, hidden_nodes=10, stddev=1/192.0, wd=0.0)
         return logits
 
